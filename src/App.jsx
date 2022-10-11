da0d2aa6e74aec2f914df364d093e78124a5e84d
gjrlwjs <shinsh@ontune.co.kr>
2022-10-11 15:16:54 +0900

    div_work_done

--- src/App.jsx
+++ src/App.jsx
@@ -5,53 +5,129 @@ import { Node } from "./Binary_tree";
 import { Binary_Tree } from "./Binary_tree";
 
 const bst = new Binary_Tree();
+let idx = 0;
 
 function App() {
+  const [arr, setArr] = useState([]);
+
   if (bst.root === null) {
-    bst.root = new Node(1, 0);
-  }
+    bst.root = new Node(idx, "N", "C", "windows1");
+    // console.log("===========arr 추가===========");
+    //arr.push(bst.root);
+    setArr([bst.root]);
+    // setIdx(idx + 1);
+    // console.log(arr);
+    // console.log("==================================================");
+  };
 
-  console.log("===========노드 2 추가===========");
+  const Add_Div = (e) => {
+    // TODO inset 계산
+    // let tmp_index = arr.findIndex((tmp_item) => tmp_item.key === e.target.id);
+    // // console.log(tmp_index);
+    // console.log('초기 Arr 확인');
+    // console.log(arr);
 
-  // parent node 구하기
+    // 0을 1|2로 Div 추가하기
+    // const node2 = bst.insert(bst.root, arr.length, arr);
+    // console.log("e 로그 = " + e.target.id);
+    const insert_result = bst.insert(arr[e.target.id], arr.length);
 
-  const node2 = bst.insert(bst.root, 2);
-  console.log(bst);
-  // bst.insert(3, 2);
-  // // bst.insert(2);
-  // // bst.insert(4);
+    if (insert_result) {
+      //arr.push(insert_result[0]);
+      //arr.push(insert_result[1]);
+      idx = idx + 2;
+      setArr([...arr, insert_result[0], insert_result[1]]);
+    };
 
-  console.log("===========노드 4 추가===========");
-  const node4 = bst.insert(node2, 4);
-  console.log(bst);
+    //setArr([...arr]);
+    // setIdx(idx + 1);
+    // console.log('현재 index = ' + idx);
+    console.log(arr);
+  };
 
-  // console.log('삭제 진행');
-  // bst.remove(3, 2);
-  // console.log(bst);
+  // filter를 써서 새 배열 만들고 arr배열을 변경해줘야함.
+  const Del_Div = (e) => {
+    // 기존 배열에서 inset 값을 변경 후 가져와야한다.
 
-  // const inset_value = [
-  //   // {inset: "0% 0% 0% 0%"}
-  //   // Level, Parent ID,
-  //   {
-  //     key: 0,
+    // setArr(arr.filter((x) => x .key!== k));
+    // console.log(arr);
+    // console.log(e);
+    if (arr[arr[arr[e.id].p_id].p_id]) {
+      bst.remove(arr[arr[arr[e.id].p_id].p_id], arr[arr[e.id].p_id], arr[e.id]);
+    } else {
+      bst.remove(null, arr[arr[e.id].p_id], arr[e.id]);
+    } 
+    console.log("삭제했음~~~");
+    setArr([...arr]);
+    console.log(arr);
+  };
 
-  //     top: 0,
-  //     right: 0,
-  //     bottom: 0,
-  //     left: 0
-  //   }
-  // ];
+  return (
+    <>
+      {arr.map((e) => {
+        // console.log(e.key);
+        // console.log(e.node_type);
 
-  // const [arr, setArr] = useState(inset_value);
-  // const [idx, setIdx] = useState(1);
+        if (e.node_type === "C") {
+          // console.log(arr);
 
-  // const Add_Div = (e) => {
+          return (
+            // 가로와 세로의 길이를 비교해서 Float 를 설정한다 (None / Left or Right)
+            <div
+              className="div_Background" key={e.id} id={e.id}
+              style={{ inset: `${e.inset_top}% ${e.inset_right}% ${e.inset_bottom}% ${e.inset_left}%` }}
+            >
+              <div className="div_Title">
+                <button onClick={Add_Div} key={e.id} id={e.id}>
+                  추가
+                </button>
+                <button onClick={()=>{Del_Div(e)}}>삭제</button>
+              </div>
+              <div className="div_Body">{e.node_text}</div>
+            </div>
+          );
+        }
+      })}
+    </>
+  );
+}
+
+export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   //   // TODO inset 계산
   //   // let tmp_index = arr.findIndex((tmp_item) => tmp_item.key === e.target.id);
+  //   // // console.log(tmp_index);
+  //   // console.log('초기 Arr 확인');
+  //   // console.log(arr);
 
-  //   // console.log(tmp_index);
-  //   console.log('초기 Arr 확인');
-  //   console.log(arr);
+  //   // 0을 1|2로 Div 추가하기
+  //   // const node2 = bst.insert(bst.root, arr.length, arr);
+  //   const insert_result = bst.insert(arr[0], arr.length);
+
+  //   if (insert_result) {
+  //     arr.push(insert_result[0]);
+  //     arr.push(insert_result[1]);
+  //     // setArr([...insert_result[1]]);
+  //     idx = idx + 2;
+  //   };
+
+
+
 
   //   //let old = arr[e.target.id];
   //   let old = arr.find(tmp_item => parseInt(tmp_item.key) === parseInt(e.target.id));
@@ -121,6 +197,89 @@ function App() {
   //     })}
   //   </>
   // );
-}
 
-export default App;
+
+
+
+  
+  // // 0을 1|2로 Div 추가하기
+  // // const node2 = bst.insert(bst.root, arr.length, arr);
+  // const insert_result = bst.insert(arr[0], arr.length);
+
+  // if (insert_result) {
+  //   arr.push(insert_result[0]);
+  //   arr.push(insert_result[1]);
+  //   // setArr([...insert_result[1]]);
+  //   idx = idx + 2;
+  // };
+
+  // // console.log("===========arr 입력 확인 ===========");
+  // // console.log(arr);
+  // // console.log("==================================================");
+
+  // // console.log("===========bst 입력 확인 ===========");
+  // // console.log(bst);
+  // // console.log("==================================================");
+
+  // // 1을 3|4로 Div 추가하기
+  // const insert_result2 = bst.insert(arr[1], arr.length);
+
+  // if (insert_result2) {
+  //   arr.push(insert_result2[0]);
+  //   arr.push(insert_result2[1]);
+  //   // setArr([...insert_result[1]]);
+  //   idx = idx + 2;
+  // };
+
+  // console.log("===========arr 입력 확인1 ===========");
+  // console.log(arr);
+  // console.log("==================================================");
+
+  // console.log("===========bst 입력 확인1 ===========");
+  // console.log(bst);
+  // console.log("==================================================");
+
+  // // 4 삭제
+  // // remove(grand_node, parent_node, del_node)
+
+  // console.log("===========4번 노드 삭제 ===========");
+  // if (arr[arr[arr[4].p_id].p_id]) {
+  //   bst.remove(arr[arr[arr[4].p_id].p_id], arr[arr[4].p_id], arr[4]);
+  // } else {
+  //   bst.remove(null, arr[arr[4].p_id], arr[4]);
+  // }
+  // console.log("==================================================");
+  
+
+  // console.log("===========arr 입력 확인2 ===========");
+  // console.log(arr);
+  // console.log("==================================================");
+
+  // console.log("===========bst 입력 확인2 ===========");
+  // console.log(bst);
+  // console.log("==================================================");
+
+
+
+
+
+
+
+
+
+
+  // const inset_value = [
+  //   // {inset: "0% 0% 0% 0%"}
+  //   // Level, Parent ID,
+  //   {
+  //     key: 0,
+
+  //     top: 0,
+  //     right: 0,
+  //     bottom: 0,
+  //     left: 0
+  //   }
+  // ];
+
+  // const [arr, setArr] = useState(inset_value);
+  // const [idx, setIdx] = useState(1);
--- src/Binary_tree.jsx
+++ src/Binary_tree.jsx
@@ -1,18 +1,29 @@
 export class Node {
   constructor(
-    id,
-    div_type = "N",
-    node_type = "P",
+    id,                           // 자신의 ID
+    div_type = "N",               // DIV 표시 방식(None / Col / Row)
+    node_type = "C",              // 노드 타입(Disable / Parent / Child )
+    node_text = "",               // Body Text
+    inset_top = 0,
+    inset_right = 0,
+    inset_bottom = 0,
+    inset_left = 0,
 
-    p_node = null,
+    p_id = null,
     left = null,
     right = null
   ) {
     this.id = id;
     this.div_type = div_type;
     this.node_type = node_type;
+    this.node_text = node_text;
 
-    this.p_node = p_node;
+    this.inset_top = inset_top;
+    this.inset_right = inset_right;
+    this.inset_bottom = inset_bottom;
+    this.inset_left = inset_left;
+
+    this.p_id = p_id;
     this.left = left;
     this.right = right;
   }
@@ -47,117 +58,164 @@ export class Binary_Tree {
     this.root = null;
   }
 
-  find_node(target_id) {
-    function searchTree(node) {
-      if (parseInt(node.id) === parseInt(target_id)) {
-        // console.log('node.id = ' + node.id);
-        // console.log('target_id = ' + target_id);
-
-        // console.log(node);
-        return node;
-      } else {
-        if (node.left) {
-          console.log("===========Left 찾음===========");
-          console.log(node);
-
-          return searchTree(node.left);
-        }
-
-        if (node.right) {
-          console.log("===========Right 찾음===========");
-          console.log(node);
-
-          return searchTree(node.right);
-        }
-      }
-
-      // console.log('node.id = ' + node.id);
-      // console.log('target_id = ' + target_id);
-    }
-
-    // console.log('node.id = ' + node.id);
-    // console.log('target_id = ' + target_id);
-
-    searchTree(this.root);
-
-    console.log("===========Left 정보===========");
-    console.log(this.root.left);
-
-    return this.root;
-  }
-
   // 추가 버튼을 통해 노드 추가 요청이 온다. 이때, 자신의 node_ID(key)를 들고 오기 때문에 Parent ID 또한 알 수 있다.
   // 기존 DIV는 좌(상)에 위치하기 때문에, 추가는 무조건 우(하)에 된다. => Right Node에 Node를 추가함.
   // 그렇다면 Left는 언제 생기냐? 추가 버튼이 눌려서 이벤트가 생성될 때, 해당 Div에 있는 key를 알 수 있으니, 그걸로 Left로 지정한다.
   // Right가 생길 때, Left가 null 일 수는 없다. 이때는 노드 자체를 삭제하고 상위 부모 자리에 Right 노드 정보를 넣어주어야한다.
   insert(parent_node, new_id) {
-    console.log("===========insert log 시작============");
+    console.log("===========insert 시작============");
 
     // 부모 node 정보를 불러와서 left, right 로 분류하여 추가해준다.(기존 Left, 신규 Right)
     // const old_node = this.find_node(parent_id);
     const old_node = parent_node;
-    const left_node = new Node(new_id, "N", "C", old_node);
-    const right_node = new Node(new_id + 1, "N", "C", old_node);
+    const left_node = new Node(new_id, "N", "C", old_node.node_text,            old_node.inset_top, old_node.inset_right, old_node.inset_bottom, old_node.inset_left, old_node.id);
+    const right_node = new Node(new_id + 1, "N", "C", "windows" + (new_id + 1), old_node.inset_top, old_node.inset_right, old_node.inset_bottom, old_node.inset_left, old_node.id);
 
-    console.log("부모 ID = " + old_node.id);
-    console.log(old_node);
+    // console.log("부모 ID = " + old_node.id);
+    // console.log(old_node);
 
     // target 노드를 찾아왔으니, Left Right 값을 입력한다.
     if (old_node) {
+      // 화면 너비
+      let calc_width = (100 - (old_node.inset_left + old_node.inset_right)) / 2;
+
+      // 화면 높이
+      let calc_height = (100 - (old_node.inset_top + old_node.inset_bottom)) / 2;
+
+      // (abs(R - L) > abs(B - T) ?)
+      if (calc_width >= calc_height) {
+        // 타입변경
+        old_node.div_type = "C";
+
+        // inset 값 셋팅
+        left_node.inset_right = old_node.inset_right + calc_width;
+        right_node.inset_left = old_node.inset_left + calc_width;
+      } else {
+        // 타입변경
+        old_node.div_type = "R";
+
+        // inset 값 셋팅
+        left_node.inset_bottom = old_node.inset_bottom + calc_height;
+        right_node.inset_top = old_node.inset_top + calc_height;
+      }
+
+      // 기존 old도 값을 바꿔주고
+      old_node.node_type = "P";
+      old_node.node_text = "";
+
+
+
+
       // Left node에 대한 내용 채워주고
       // left_node.node_type = "C";
 
       // Right node에 대한 내용 채워주고
       // right_node.node_type = "C";
 
-      // 기존 old도 값을 바꿔주고
-      old_node.node_type = "P";
-
       // Left | right 입력
-      old_node.left = left_node;
+      // old_node.left_id  = left_node.id;
+      // old_node.right_id = right_node.id;
+      old_node.left  = left_node;
       old_node.right = right_node;
+
+      return [left_node, right_node];//[left_node, left_node, right_node];
     }
-    return old_node.left;
+    return null;//left_node;//old_node.left;
+    // return left_node;//old_node.left;
   }
 
-  // remove(data) {
-  //   //제거할 data의 파라미터를 두번째에 놓았다.
-  //   const removeNode = function (node, data) {
-  //     if (node == null) {
-  //       return null;
-  //     }
-  //     if (data == node.data) {
-  //       // node has no children ~ 밑에 뿌리가 없는 노드
-  //       if (node.left == null && node.right == null) {
-  //         return null;
-  //       }
-  //       // node has no left child  ~ left는 없는 경우 node right가 해당 삭제 데이터에 들어간다.
-  //       if (node.left == null) {
-  //         return node.right;
-  //       }
-  //       // node has no right child
-  //       if (node.right == null) {
-  //         return node.left;
-  //       }
-  //       // node has two children
-  //       var tempNode = node.right;
-  //       //tempNode는 삭제할 node의 right가 되고
-  //       while (tempNode.left !== null) {
-  //         tempNode = tempNode.left; //다시 node right의 left가 된다.
-  //       }
-  //       node.data = tempNode.data; //그리고 삭제 node에는 위의 tempnode가 들어가게된다.
-  //       node.right = removeNode(node.right, tempNode.data);
-  //       return node;
-  //     } else if (data < node.data) {
-  //       node.left = removeNode(node.left, data);
-  //       return node;
-  //     } else {
-  //       node.right = removeNode(node.right, data);
-  //       return node;
-  //     }
-  //   }
-  //   this.root = removeNode(this.root, data);
-  // }
+  // 삭제시, 
+  // 자신과 형제노드 - 부모노드 간의 연결을 해제하고
+  // 조부모노드와 자신의 정보를 연결해준다.
+  remove(grand_node, parent_node, del_node) {
+    // 조부모 노드의 존재 여부에 따라 로직이 달라짐
+    if (grand_node) {
+
+      // 형제 노드의 p_id를 바꿔준다.
+      // 삭제할 노드의 위치가 Left?
+      if (parent_node.left.id === del_node.id) {
+        parent_node.right.p_id = grand_node.id;
+
+        // 부모의 inset 값을 형제노드에 대입해준다.
+        parent_node.right.inset_top    = parent_node.inset_top;
+        parent_node.right.inset_right  = parent_node.inset_right;
+        parent_node.right.inset_bottom = parent_node.inset_bottom;
+        parent_node.right.inset_left   = parent_node.inset_left;
+
+        //parent_node.right.node_text    = parent_node.node_text;
+
+        // 삭제할 노드의 부모노드가 조부모노드 기준 L? R? 체크
+        if (grand_node.left.id === parent_node.id) {
+          grand_node.left = parent_node.right;
+        } else {
+          grand_node.right = parent_node.right;
+        }
+      } else {
+        parent_node.left.p_id = grand_node.id;
+
+        // 부모의 inset 값을 형제노드에 대입해준다.
+        parent_node.left.inset_top    = parent_node.inset_top;
+        parent_node.left.inset_right  = parent_node.inset_right;
+        parent_node.left.inset_bottom = parent_node.inset_bottom;
+        parent_node.left.inset_left   = parent_node.inset_left;    
+        
+        //parent_node.left.node_text    = parent_node.node_text;        
+
+        // 삭제할 노드의 부모노드가 조부모노드 기준 L? R? 체크
+        if (grand_node.left.id === parent_node.id) {
+          grand_node.left = parent_node.left;
+        } else {
+          grand_node.right = parent_node.left;
+        }          
+      }
+    } else {
+      // 삭제할 노드의 위치가 Left?
+      if (parent_node.left.id === del_node.id) {
+        parent_node.right.p_id = null;
+
+        // 부모의 inset 값을 형제노드에 대입해준다.
+        parent_node.right.inset_top    = parent_node.inset_top;
+        parent_node.right.inset_right  = parent_node.inset_right;
+        parent_node.right.inset_bottom = parent_node.inset_bottom;
+        parent_node.right.inset_left   = parent_node.inset_left;      
+        
+        //parent_node.right.node_text    = parent_node.node_text;
+      } else {
+        parent_node.left.p_id = null;
+
+        // 부모의 inset 값을 형제노드에 대입해준다.
+        parent_node.left.inset_top    = parent_node.inset_top;
+        parent_node.left.inset_right  = parent_node.inset_right;
+        parent_node.left.inset_bottom = parent_node.inset_bottom;
+        parent_node.left.inset_left   = parent_node.inset_left;        
+        
+        //parent_node.left.node_text    = parent_node.node_text;            
+      }
+      // // 부모노드와 삭제노드의 Type을 D(Delete)로 바꿔주고 형제노드 정보의 부모ID를 null로 바꾼다.(Root가 된다)
+      // // del_node.p_id = -1;
+      // del_node.node_type = "D";
+
+      // // parent_node.p_id  = -1;
+      // // parent_node.div_type = "N";
+      // parent_node.node_type = "D";
+
+      // parent_node.left  = null;
+      // parent_node.right = null;
+    }
+
+    // 부모노드와 삭제노드의 Type을 D(Delete)로 바꿔주고 형제노드 정보의 부모ID를 null로 바꾼다.(Root가 된다)
+    // del_node.p_id = -1;
+    del_node.node_type = "D";
+
+    // parent_node.p_id  = -1;
+    // parent_node.div_type = "N";
+    parent_node.node_type = "D";
+
+    parent_node.left  = null;
+    parent_node.right = null;    
+    // this.root = removeNode(this.root, del_id);
+    return;
+  }
 }
 
 // export class Binary_Tree {
@@ -267,3 +325,45 @@ export class Binary_Tree {
 // }
 
 //export default Binary_Tree Node;
+
+
+
+
+// find_node(target_id) {
+//   function searchTree(node) {
+//     if (parseInt(node.id) === parseInt(target_id)) {
+//       // console.log('node.id = ' + node.id);
+//       // console.log('target_id = ' + target_id);
+
+//       // console.log(node);
+//       return node;
+//     } else {
+//       if (node.left) {
+//         console.log("===========Left 찾음===========");
+//         console.log(node);
+
+//         return searchTree(node.left);
+//       }
+
+//       if (node.right) {
+//         console.log("===========Right 찾음===========");
+//         console.log(node);
+
+//         return searchTree(node.right);
+//       }
+//     }
+
+//     // console.log('node.id = ' + node.id);
+//     // console.log('target_id = ' + target_id);
+//   }
+
+//   // console.log('node.id = ' + node.id);
+//   // console.log('target_id = ' + target_id);
+
+//   searchTree(this.root);
+
+//   console.log("===========Left 정보===========");
+//   console.log(this.root.left);
+
+//   return this.root;
+// }
